9c14870ae79fdc8ff5126fbab46daaa303a474e3
thinkpad <1027918160@qq.com>
2018-10-27 22:42:36 +0800

    dd

--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -54,9 +54,9 @@ return array(
 
     /* 数据库设置 */
     'DB_TYPE'                => 'mysql', // 数据库类型
-    'DB_HOST'                => '43.227.195.86', // 服务器地址
+    'DB_HOST'                => '101.132.110.155', // 服务器地址
     'DB_NAME'                => 'purse', // 数据库名
-    'DB_USER'                => 'root', // 用户名
+    'DB_USER'                => 'root1', // 用户名
     'DB_PWD'                 => 'qaz13067972109', // 密码
     'DB_PORT'                => '3306', // 端口
     'DB_PREFIX'              => 'yt_', // 数据库表前缀
