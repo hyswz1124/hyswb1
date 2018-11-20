5f052777effb8efcd69e878731198985b5625f5b
thinkpad <1027918160@qq.com>
2018-11-20 17:40:22 +0800

    dd

--- App/Common/Common/function.php
+++ App/Common/Common/function.php
@@ -405,8 +405,8 @@ function airdrop_reward($user_id){
            $payment['status'] = 1;
            M('payments')->add($payment);
            M('users')->where("id =".$user_id)->save(['eth' => ($owner['eth']) + $trade['eth'],'paradrop_earnings'=>($owner['paradrop_earnings'] + $trade['eth']),'is_kt'=>0, 'update_time' =>date('Y-m-d H:i:s',time())]);
-           return 13;
            M('bonus_pool')->where('type = 3')->save(['eth'=>($amount - $trade['eth']),'update_time' =>date('Y-m-d H:i:s',time())]);
+           return 14;
        }
        return true;
 }
