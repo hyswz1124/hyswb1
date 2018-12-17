9ae305a2790bca159483a88fa7fc84d9f1977a40
swz <wz123>
2018-12-17 12:58:40 +0800

    111

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -2,5 +2,5 @@
 return array(
 	//'配置项'=>'配置值'
     'APPHOST'               => 'http://101.132.110.155/',
-    'INVITEHOST'               => 'http://101.132.110.155/index.php/invite?id=',
+    'INVITEHOST'               => 'http://101.132.110.155/home/index/Invite?id=',
 );
